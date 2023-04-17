b9812d01bc4f021459591bb9362c512fe0de6df4
Samuel Jin <115764167+SekretAgent@users.noreply.github.com>
2023-04-16 18:36:58 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+function getData() {firebase.database().ref("/").on('value', function (snapshot) {document.getElementById("output").innerHTML
+"";snapshot.forEach(function(childSnapshot) {childKey
+childSnapshot.key;
+Room names = childKey;
+//Start code
+row = "<div class 'room_name' id = "+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+//End code
+}); }); }
+getData();
+
